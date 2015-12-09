eaa5bbdf9fef413a3f591971096b9995a93460e6
Dominic <dominiccanora@gmail.com>
2015-12-08 21:05:09 -0500

    Ready for prime time!

--- lightcontrol.py
+++ lightcontrol.py
@@ -1,23 +1,30 @@
 import threading
 import opc
-import random
+from random import randint
+import time
+import math
+from math import *
 import time
 
 GRG_LEN = 150
 PERIOD = 1024
+SLEEP = 0.01
 
 pixels = []
 
-j = lambda: int(round(time.time() * 1000)) % PERIOD
-time = lambda: int(round(time.time() * 1000))
+j = lambda: int(round(time.time() * 10)) % PERIOD
+theTime = lambda: int(round(time.time() * 10))
 
 
 class LightController(threading.Thread):
-    def __init__(self, ip='127.0.0.1:7890'):
+    
+    def __init__(self, ip='192.168.0.123:7890'):
         threading.Thread.__init__(self)
         
         self.fc = opc.Client(ip)
         self.state = 0
+        self.direction = True
+        self.position = 0
 
         # Test if it can connect (optional)
         if self.fc.can_connect():
@@ -34,23 +41,52 @@ class LightController(threading.Thread):
 
     def run(self):
         while(True):
-            #print self.state
-            pass
+        #Kinetics
+            if self.state == 1:
+                self.fade()
+            elif self.state == 2:
+                self.wipe()
+            elif self.state == 3:
+                self.twinkle()
+            elif self.state == 4:
+                self.redGreenTwinkle()
+            elif self.state == 5:
+                self.cylon()
+        #Statics
+            elif self.state == 100:
+                self.redGreenStatic()
+            elif self.state == 101:
+                self.whiteStatic()
+        #Finally
+            else:
+                self.allOff()
+
+            time.sleep(SLEEP)
+
+    def allOff(self):
+        pixels = []
+        for i in range(GRG_LEN):
+            pixels.append((0,0,0))
+        self.fc.put_pixels(pixels)
 
     def fade(self):
         pixels = []
-        for i in range(GRG_LEN / 2):
-            sin = 255 * (0.5 * math.sin(float(j) * 360.0 / float(PERIOD)))
-            sin2 = 255 * (0.5 * math.sin(math.pi + (float(j) * 360.0 / float(PERIOD))))
+        for i in range(int(math.ceil(GRG_LEN / 6))):
+            sin = 255 * (0.5 * math.sin(j() * 180.0 / PERIOD))
+            sin2 = 255 * (0.5 * math.sin(math.pi + (j() * 180.0 / PERIOD)))
 
             pixels.append((sin, sin2, 0))
+            pixels.append((sin, sin2, 0))
+            pixels.append((0, 0, 0))
             pixels.append((sin2, sin, 0))
+            pixels.append((sin2, sin, 0))
+            pixels.append((0, 0, 0))
 
         self.fc.put_pixels(pixels)
 
     def wipe(self):
         pixels = []
-        k = time % (GRG_LEN * 2)
+        k = (theTime() % (GRG_LEN * 2))
         for i in range(GRG_LEN):
             if k < GRG_LEN:
                 if i < k:
@@ -58,7 +94,7 @@ class LightController(threading.Thread):
                 else:
                     pixels.append((0, 255, 0))
             else:
-                if (GRG_LRN + i) < k:
+                if (GRG_LEN + i) < k:
                     pixels.append((0, 255, 0))
                 else:
                     pixels.append((255, 0, 0))
@@ -71,7 +107,86 @@ class LightController(threading.Thread):
                 pixels.append((255, 255, 175))
             else:
                 pixels.append((128, 140, 65))
+        self.fc.put_pixels(pixels)
 
+        time.sleep(0.04)
 
+    def redGreenTwinkle(self):
+        pixels = []
+        for i in range(int(math.ceil(GRG_LEN / 6))):
+            pixels.append((255, 0, 0))
+            #pixels.append((255, 0, 0))
 
+            if(randint(0, 10000) < 20):
+                pixels.append((255, 255, 175))
+            else:
+                pixels.append((128, 140, 65))
+
+            if(randint(0, 10000) < 20):
+                pixels.append((255, 255, 175))
+            else:
+                pixels.append((128, 140, 65))
                 
+            pixels.append((0, 255, 0))
+            #pixels.append((0, 255, 0))
+
+            if(randint(0, 10000) < 20):
+                pixels.append((255, 255, 175))
+            else:
+                pixels.append((128, 140, 65))
+
+            if(randint(0, 10000) < 20):
+                pixels.append((255, 255, 175))
+            else:
+                pixels.append((128, 140, 65))
+
+        self.fc.put_pixels(pixels)
+
+    def cylon(self):
+        pixels = []
+        for i in range(GRG_LEN):
+            pixels.append((0, 0, 0))
+
+        try:
+            pixels[self.position] = ((128, 0, 0))
+            pixels[self.position + 1] = ((0, 255, 0))
+            pixels[self.position + 2] = ((128, 0, 0))
+        except IndexError:
+            self.direction = not self.direction
+
+        self.fc.put_pixels(pixels)
+
+        if self.position <= 1:
+            self.direction = True
+        
+        if self.direction:
+            self.position += 1
+        else:
+            self.position -= 1
+
+        time.sleep(0.01)
+            
+
+#Statics
+
+    def redGreenStatic(self):
+        pixels = []
+        for i in range(int(math.ceil(GRG_LEN / 6))):
+            pixels.append((255, 0, 0))
+            pixels.append((255, 0, 0))
+
+            pixels.append((0, 0, 0))
+                
+            pixels.append((0, 255, 0))
+            pixels.append((0, 255, 0))
+
+            pixels.append((0, 0, 0))
+        self.fc.put_pixels(pixels)
+
+    def whiteStatic(self):
+        pixels = []
+        for i in range(GRG_LEN):
+            pixels.append((255, 255, 175))
+        self.fc.put_pixels(pixels)
+            
+
--- runme.py
+++ runme.py
@@ -9,6 +9,21 @@ def setState(newstate = 0):
     setstateifint(newstate)
     return newstate
 
+@route('/opc/getstates')
+def listStates():
+    return '''
+{
+    0 : "All Off",
+    1 : "Red/Green Fade",
+    2 : "Red/Green Wipe",
+    3 : "White Twinkle",
+    4 : "Red/Green/White Twinkle",
+    5 : "Christmas Cylon",
+    100 : "Static Red/Green",
+    101 : "White Static"
+}
+    '''
+
 def setstateifint(newstate):
     try:
         statetoset = int(newstate)
