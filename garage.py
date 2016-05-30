7ee92065987a83045db9d4bdb34a7ae441e2d778
Dominic Canora <dominiccanora@gmail.com>
2016-05-30 11:33:17 -0400

    Now red white blue instead of blue white red

--- garage.py
+++ garage.py
@@ -57,7 +57,7 @@ class GarageController(threading.Thread):
         self.specialdays[(3, 27)] = ((255, 141, 161), (255, 255, 128), (192, 255, 244))  # Easter YEARLY
         self.specialdays[(4, 2)] = ((0, 170, 255), (255, 255, 175))  # Light it up blue
         self.specialdays[(5, 5)] = ((255, 0, 0), (0, 255, 0), (255, 255, 175))  # Cinco de Mayo
-        self.specialdays[(5, 30)] = ((255, 0, 0), (255, 255, 175), (0, 0, 255)) # Memorial Day YEARLY
+        self.specialdays[(5, 30)] = ((0, 0, 255), (255, 255, 175), (255, 0, 0)) # Memorial Day YEARLY
         self.specialdays[(7, 4)] = ((255, 0, 0), (255, 255, 175), (0, 0, 255))  # Independence Day
         self.specialdays[(11, 11)] = ((255, 0, 0), (255, 255, 175), (0, 0, 255)) # Veteran's Day
 
