6d75d727a1ebcf4d57d0b4461520146249745133
Dominic Canora <dominiccanora@gmail.com>
2017-05-20 06:53:57 -0400

    Graduation

--- garage.py
+++ garage.py
@@ -63,6 +63,7 @@ class GarageController(threading.Thread):
         self.specialdays[(4, 2)] = ((0, 170, 255), (255, 255, 175))  # Light it up blue
         self.specialdays[(4, 16)] = ((255, 141, 161), (255, 255, 128), (192, 255, 244))  # Easter YEARLY
         self.specialdays[(5, 5)] = ((255, 0, 0), (0, 255, 0), (255, 255, 175))  # Cinco de Mayo
+        self.specialdays[(5, 20)] = ((255, 0, 0), (255, 0, 0), (255, 255, 175), (255, 255, 175)) # Graduation
         self.specialdays[(5, 29)] = ((0, 0, 255), (255, 255, 175), (255, 0, 0)) # Memorial Day YEARLY
         self.specialdays[(7, 4)] = ((255, 0, 0), (255, 255, 175), (0, 0, 255))  # Independence Day
         self.specialdays[(10, 31)] = ((150, 0, 128), (255, 165, 0)) #Halloween
