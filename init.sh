60e364847200e30a2b515fb634d3aa9e1a67cbb0
Dominic Canora <dominiccanora@gmail.com>
2018-10-28 17:13:50 +0000

    Update init script

--- init.sh
+++ init.sh
@@ -1,1 +1,2 @@
-/usr/local/bin/fcserver /home/pi/fadecandy-xmas/server-config.json >/var/log/fcserver.log 2>&1 &
+/usr/local/bin/fcserver /home/pi/fadecandy-xmas/server-config.json > /var/log/fcserver.log 2>&1 &
+python /home/pi/fadecandy-xmas/runme.py >/var/log/fadecandy-xmas.log 2>&1 &
